179647720592949b9a5aaa55dc5603435eb34b08
Aaron <898aarsing@gmail.com>
2022-01-29 21:18:12 -0800

    Update main.js

--- main.js
+++ main.js
@@ -41,15 +41,7 @@ function closemagnify(){
         $("#magnify").removeClass("animated fadeOut");
         $("#img_here").css("background",`url('') center center`);
     },800);
-}
 
-setTimeout(function(){
-    $("#loading").addClass("animated fadeOut");
-    setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
-    },800);
-},1650);
 
 $(document).ready(function(){
     $("a").on('click', function(event) {
